cba176ac46ab88864e6a91c8e65a63a3fc69c075
soziax <89212948+soziax@users.noreply.github.com>
2024-05-22 14:25:05 +0300

    making a simple white 800 by 800 window.

--- main.cpp
+++ main.cpp
@@ -4,8 +4,26 @@
 using namespace std;
 int main()
 {
-	cout << "Working";
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Opengl learning", NULL, NULL);
+	if (window == NULL)
+	{
+		cout << "Failed to create GLFW window\n";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
 
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
